431c66625f70db3d7a204b74e47823293cec72b1
MollikaComputer Provider <mollikacomputer3@gmail.com>
2024-04-17 00:14:16 +0600

    implement deleted method

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongodb = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 // mongodb atlas
@@ -31,17 +32,83 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Crud2 server. You successfully connected to MongoDB!");
 
+    // const coffeeCollection = client.db('crud2').collection('coffee');
     const coffeeCollection = client.db('crud2').collection('coffee');
-
-
-    // post data client side to server side
+    // const coffeeCollection = client.db('crud2').collection('coffee');
     app.post('/coffee', async(req, res)=>{
       const newCoffee = req.body;
-      console.log(newCoffee);
-
       const result = await coffeeCollection.insertOne(newCoffee);
-      res.send(result)
+      res.send(result);
     });
+
+    app.get('/coffee', async(req, res)=>{
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    
+    app.delete('/coffee/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    })
+    // app.post('/coffee', async(req, res)=>{
+    //   const newCoffee = req.body;
+    //   const result = await coffeeCollection.insertOne(newCoffee);
+    //   res.send(result);
+    // });
+
+    // app.get('/coffee', async(req, res)=>{
+    //   const cursor = coffeeCollection.find();
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // });
+
+
+    // app.post('/coffee', async(req, res)=>{
+    //   const newCoffee = req.body;
+    //   const result = await coffeeCollection.insertOne(newCoffee);
+    //   res.send(result);
+    // });
+    // app.get('/coffee', async(req, res)=>{
+    //   const cursor = coffeeCollection.find();
+    //   const result = await cursor.toArray();
+    //   res.send(result)
+    // });
+    
+    // app.post('/coffee', async(req, res)=>{
+    //   const newCoffee = req.body;
+    //   const result = await coffeeCollection.insertOne(newCoffee);
+    //   res.send(result);
+    // });
+
+    // app.get('/coffee', async(req, res)=>{
+    //   const cursor = coffeeCollection.find();
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // });
+    // app.post('/coffee', async(req, res)=>{
+    //   const newCoffee = req.body;
+    //   const result = await coffeeCollection.insertOne(newCoffee);
+    //   res.send(result);
+    // });
+
+    // app.get('/coffee', async(req, res)=>{
+    //   const cursor = coffeeCollection.find();
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // });
+    
+    // app.post('/coffee', async(req, res)=>{
+    //   const newCoffee = req.body;
+    //   const result = await coffeeCollection.insertOne(newCoffee);
+    //   res.send(result)
+    // });
+
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
